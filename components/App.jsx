74c6ddd1d92dfe01803e31534161d31fde621614
Raziel <robroblemol>
2019-03-29 13:47:06 -0500

    Routing Components

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,25 @@
-import React,{Component} from 'react'
+import React,{ Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+ 
 
 import Home from './Home'
+import Contact from './Contact'
+import About from './About'
 import '../style.css'
 
 export default class App extends Component {
     render(){
         return (
         <div id = "app-container">
-            <Home />
+            <Router>
+                <div>
+                    {/* con el parametro exact para que no sigamostrando el home */}
+                    <Route exact path="/" component = {Home}/>
+                    <Route path="/contact" component = {Contact}/>
+                    <Route path="/about" component = {About}/>
+
+                </div>
+            </Router>
         </div>
         )
     }
