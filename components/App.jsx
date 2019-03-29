6a70822b0a8e405ca6caf3fb5127d2a1c72bc69d
Raziel <robroblemol>
2019-03-29 12:20:52 -0500

    frist commit

---
+++ components/App.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+
+
+export default class App extends Component {
+    render(){
+        return (
+        <div id = "app-container">
+            <h1>Mi Aplicacion</h1>
+            <p>bienvenido a mi aplicación.</p>
+        </div>
+        )
+    }
+}
